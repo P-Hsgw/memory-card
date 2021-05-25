648d7ca9bf02ea0a2e336f8b2a5a5997034ce84f
P-Hsgw <contact@p-hsgw.com>
2021-05-25 14:56:54 +0900

    Add randomeness with CSS Flex

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./Card.module.css";
+import "./Card-random.css"
 
 function Card({ img, name }) {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} card-${Math.floor(Math.random()*8)}`}>
       <img src={require(`../images/${img}.webp`).default} />
       <p>NAME: {name}</p>
     </div>
