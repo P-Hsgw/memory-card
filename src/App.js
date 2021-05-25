3901ca585442587baab743ab6faf6cebe34632ca
P-Hsgw <contact@p-hsgw.com>
2021-05-25 11:29:28 +0900

    Update basic page structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import styles from "./App.module.css"
 
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Cards from "./components/Cards"
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Cards.module.css"
+
+function Cards () {
+
+  return (
+    <div className={styles.container}>
+      TESTUWKA
+    </div>
+  )
+}
+
+export default Cards
