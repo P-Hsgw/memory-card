f1b4002efc468fc6b5d5260b5faf8052738571c2
P-Hsgw <contact@p-hsgw.com>
2021-05-25 17:40:21 +0900

    Prepare a function to keep clicks in useState

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useState } from "react"
 import styles from "./Card.module.css";
 import "./Card-random.css"
 
 function Card({ img, name, id }) {
+  const [ clicked, setClicked ] = useState(false)
+
+
+  function randomizeCards() {
+    return Math.floor(Math.random()*9)
+  }
+
+  function handleClick (e) {
+    setClicked(!clicked)
+    console.log(e.currentTarget.dataset.id)
+  }
+
   return (
-    <div className={`${styles.container} card-${Math.floor(Math.random()*8)} data-id-${id}`}>
-      <img src={require(`../images/${img}.webp`).default} />
+    <div data-id={id} className={`${styles.container} card-${randomizeCards()} `} onClick={handleClick}>
+      <img src={require(`../images/${img}.webp`).default} alt={name}/>
       <p>NAME: {name}</p>
     </div>
   );
