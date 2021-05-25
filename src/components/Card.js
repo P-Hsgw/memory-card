4b8a23bbb8f9ce814f4291f55801b2cb2e5c9b43
P-Hsgw <contact@p-hsgw.com>
2021-05-25 18:42:11 +0900

    Move card ID to state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react"
 import styles from "./Card.module.css";
 import "./Card-random.css"
 
 function Card({ img, name, id }) {
   const [ clicked, setClicked ] = useState(false)
+  const [ cardId, setCardId ] = useState(undefined)
 
+  // Trzymac numer card- w stanie i aktualizowac go przy kazdym kliknieciu
+  // Przy renderze generuje sie jeden numer od 0 do 9
+  // Przy kliknieciu sie zmienia
 
-  function randomizeCards() {
-    return Math.floor(Math.random()*9)
-  }
+  useEffect(() => {
+    setCardId(Math.floor(Math.random()*9)) 
+  }, [])
+
 
   function handleClick (e) {
     setClicked(!clicked)
-    console.log(e.currentTarget.dataset.id)
+    setCardId(Math.floor(Math.random()*9))
+
   }
 
   return (
-    <div data-id={id} className={`${styles.container} card-${randomizeCards()} `} onClick={handleClick}>
+    <div data-id={id} className={`${styles.container} card-${cardId}`} onClick={handleClick}>
       <img src={require(`../images/${img}.webp`).default} alt={name}/>
       <p>NAME: {name}</p>
     </div>
