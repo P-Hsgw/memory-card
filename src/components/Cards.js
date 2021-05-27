2967b17b46f4813f90ce8d9ba6c61f7fe16ff805
P-Hsgw <contact@p-hsgw.com>
2021-05-27 09:58:12 +0900

    Improve logic for calculating score

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,26 +6,37 @@ import Card from "./Card";
 import { characters } from "./Characters";
 
 function Cards() {
-  const [ currentScore, setCurrentScore ] = useState(0)
-  const [ totalScore, setTotalScore ] = useState(0)
-  const [ clickedId, setClickedId ] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [topScore, setTopScore] = useState(0);
+  const [clickedId, setClickedId] = useState([]);
 
-  function handleScore (currentId) {
-    if(clickedId.includes(currentId)) {
-      setTotalScore(currentScore)
-      setCurrentScore(0)
-      setClickedId([])
+  function handleScore(currentId) {
+    if (clickedId.includes(currentId) && topScore < currentScore) {
+      setTopScore(currentScore);
+      setCurrentScore(0);
+      setClickedId([]);
+    } else if (clickedId.includes(currentId)) {
+      setCurrentScore(0);
+      setClickedId([]);
     } else {
-      setCurrentScore(currentScore+1)
-      setClickedId([...clickedId, currentId])
+      setCurrentScore(currentScore + 1);
+      setClickedId([...clickedId, currentId]);
     }
   }
 
   return (
     <div className={styles.container}>
-        {characters.map((item, i) => {
-          return <Card key={i} name={item.name} img={item.img} id={i} click={handleScore}/>;
-        })}
+      {characters.map((item, i) => {
+        return (
+          <Card
+            key={i}
+            name={item.name}
+            img={item.img}
+            id={i}
+            click={handleScore}
+          />
+        );
+      })}
     </div>
   );
 }
